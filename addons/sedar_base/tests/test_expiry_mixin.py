a48cf28c4ff652a07b2cb31086db8c88646bd8d1
MannLester <mannlesterm@gmail.com>
2026-07-06 01:43:23 +0800

    feat(sedar_base): add shared expiry-tracking mixin

---
+++ addons/sedar_base/tests/test_expiry_mixin.py
@@ -0,0 +1,25 @@
+from datetime import timedelta
+
+from odoo.fields import Date
+from odoo.tests.common import TransactionCase
+
+
+class TestExpiryMixin(TransactionCase):
+
+    def _make_concrete_model_record(self, expiry_date):
+        return self.env['sedar.hsse.permit'].create({
+            'name': 'Test Permit',
+            'expiry_date': expiry_date,
+        })
+
+    def test_expired_status(self):
+        rec = self._make_concrete_model_record(Date.today() - timedelta(days=1))
+        self.assertEqual(rec.expiry_status, 'expired')
+
+    def test_warning_status(self):
+        rec = self._make_concrete_model_record(Date.today() + timedelta(days=10))
+        self.assertEqual(rec.expiry_status, 'warning')
+
+    def test_ok_status(self):
+        rec = self._make_concrete_model_record(Date.today() + timedelta(days=365))
+        self.assertEqual(rec.expiry_status, 'ok')
