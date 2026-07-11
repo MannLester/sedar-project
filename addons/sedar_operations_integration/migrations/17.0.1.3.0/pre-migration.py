4fb12cfa4988d72e67e09e58ef499b7e59b2c940
MannLester <mannlesterm@gmail.com>
2026-07-11 22:34:34 +0800

    fix: stabilize document renewal upgrade

--- addons/sedar_operations_integration/migrations/17.0.1.3.0/pre-migration.py
+++ addons/sedar_operations_integration/migrations/17.0.1.3.0/pre-migration.py
@@ -7,6 +7,18 @@ def migrate(cr, version):
     )
     cr.execute(
         """
+        SELECT data_type
+        FROM information_schema.columns
+        WHERE table_name = 'sedar_document_control'
+          AND column_name = 'renewal_owner'
+        """
+    )
+    row = cr.fetchone()
+    if not row or row[0] not in ('character varying', 'text'):
+        return
+
+    cr.execute(
+        """
         UPDATE sedar_document_control
         SET legacy_renewal_owner = renewal_owner
         WHERE renewal_owner IS NOT NULL
